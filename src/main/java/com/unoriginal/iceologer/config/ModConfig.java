47403f07b8cc2426085080d831be64c9b0f8c93e
Un-Original1 <78579980+Un-Original1@users.noreply.github.com>
2021-02-06 12:06:19 -0500

    Add files via upload

---
+++ src/main/java/com/unoriginal/iceologer/config/ModConfig.java
@@ -0,0 +1,39 @@
+package com.unoriginal.iceologer.config;
+
+import com.unoriginal.iceologer.Main;
+import com.unoriginal.iceologer.proxy.ServerProxy;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Configuration;
+import org.apache.logging.log4j.Level;
+
+@Config(modid = Main.MODID)
+@Config.LangKey("iceologer.config.title")
+public class ModConfig {
+
+    public static boolean isIceologerEnabled = true;
+    public static boolean isIceWandEnabled = true;
+    public static int iceologerSpawnProbability = 5;
+
+    private static final String CATEGORY_GENERAL = "general";
+    public static void readConfig() {
+        Configuration cfg = ServerProxy.config;
+        try {
+            cfg.load();
+            initGeneralConfig(cfg);
+        } catch (Exception e1) {
+            Main.logger.log(Level.ERROR, "Problem loading config file!", e1);
+        } finally {
+            if (cfg.hasChanged()) {
+                cfg.save();
+            }
+        }
+    }
+
+    private static void initGeneralConfig(Configuration cfg) {
+        cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
+        // cfg.getBoolean() will get the value in the config if it is already specified there. If not it will create the value.
+        isIceologerEnabled = cfg.getBoolean("isIceologerEnabled", CATEGORY_GENERAL, isIceologerEnabled, "Set to false if you want to disable iceologer from the mod");
+        isIceWandEnabled = cfg.getBoolean("isIceWandEnabled", CATEGORY_GENERAL, isIceWandEnabled, "Set to false if you want to disable Ice wand from the mod");
+        iceologerSpawnProbability = cfg.getInt("iceologerSpawnProbability", CATEGORY_GENERAL, iceologerSpawnProbability, 0, 100, "Change the spawn rate of iceologer, 0 to disable it and 100 to make them as common as a zombie");
+    }
+}
