7cb31f2498228e17361f8ca907aaaf9ef99e1b17
Un-Original1 <78579980+Un-Original1@users.noreply.github.com>
2021-02-11 16:43:11 -0500

    Update ModConfig.java

--- src/main/java/com/unoriginal/iceologer/config/ModConfig.java
+++ src/main/java/com/unoriginal/iceologer/config/ModConfig.java
@@ -13,6 +13,8 @@ public class ModConfig {
     public static boolean isIceologerEnabled = true;
     public static boolean isIceWandEnabled = true;
     public static int iceologerSpawnProbability = 5;
+    public static int iceologerMinSpawnGroup= 1;
+    public static int iceologerMaxSpawnGroup= 1;
 
     private static final String CATEGORY_GENERAL = "general";
     public static void readConfig() {
@@ -35,5 +37,7 @@ public class ModConfig {
         isIceologerEnabled = cfg.getBoolean("isIceologerEnabled", CATEGORY_GENERAL, isIceologerEnabled, "Set to false if you want to disable iceologer from the mod");
         isIceWandEnabled = cfg.getBoolean("isIceWandEnabled", CATEGORY_GENERAL, isIceWandEnabled, "Set to false if you want to disable Ice wand from the mod");
         iceologerSpawnProbability = cfg.getInt("iceologerSpawnProbability", CATEGORY_GENERAL, iceologerSpawnProbability, 0, 100, "Change the spawn rate of iceologer, 0 to disable it and 100 to make them as common as a zombie");
+        iceologerMinSpawnGroup = cfg.getInt("iceologerMinSpawnGroup", CATEGORY_GENERAL, iceologerMinSpawnGroup, 1, 3, "Change the minimum of iceologers spawned in a group");
+        iceologerMaxSpawnGroup = cfg.getInt("iceologerMaxSpawnGroup", CATEGORY_GENERAL, iceologerMaxSpawnGroup, 1, 3, "Change the maximum of iceologers spawned in a group");
     }
 }
