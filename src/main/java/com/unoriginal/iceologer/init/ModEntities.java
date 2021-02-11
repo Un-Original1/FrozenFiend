f961689d1334724d19ca9abb6ed4021fb4480a9c
Un-Original1 <78579980+Un-Original1@users.noreply.github.com>
2021-02-11 16:46:00 -0500

    Update ModEntities.java

--- src/main/java/com/unoriginal/iceologer/init/ModEntities.java
+++ src/main/java/com/unoriginal/iceologer/init/ModEntities.java
@@ -1,6 +1,8 @@
 package com.unoriginal.iceologer.init;
 
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.unoriginal.iceologer.Main;
 import com.unoriginal.iceologer.config.ModConfig;
 import com.unoriginal.iceologer.entity.Entity.EntityIceCube;
@@ -8,8 +10,10 @@ import com.unoriginal.iceologer.entity.Entity.EntityIceologer;
 import com.unoriginal.iceologer.entity.render.RenderIceCube;
 import com.unoriginal.iceologer.entity.render.RenderIceologer;
 import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.init.Biomes;
 import net.minecraft.util.ResourceLocation;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -23,8 +27,17 @@ public class ModEntities
             if(ModConfig.isIceologerEnabled == true) {
                 EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID, "Iceologer"), EntityIceologer.class, "Iceologer", id++, Main.instance, 70, 3, true, 128, 14078143);
             }
-            EntityRegistry.addSpawn(EntityIceologer.class, ModConfig.iceologerSpawnProbability, 1, 1, EnumCreatureType.MONSTER, Biomes.FROZEN_RIVER, Biomes.FROZEN_OCEAN, Biomes.ICE_MOUNTAINS, Biomes.ICE_PLAINS, Biomes.MUTATED_ICE_FLATS, Biomes.EXTREME_HILLS, Biomes.EXTREME_HILLS_WITH_TREES, Biomes.MUTATED_EXTREME_HILLS, Biomes.MUTATED_EXTREME_HILLS_WITH_TREES, Biomes.MUTATED_TAIGA_COLD, Biomes.COLD_TAIGA, Biomes.COLD_TAIGA_HILLS, Biomes.COLD_BEACH);
-            EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID,"IceCube"), EntityIceCube.class, "IceCube", id++, Main.instance, 70, 3,true);
+        Multimap<BiomeDictionary.Type, Biome> biomesAndTypes = HashMultimap.create();
+            for (Biome b : Biome.REGISTRY)
+            {
+            Set<BiomeDictionary.Type> types = BiomeDictionary.getTypes(b);
+            for (BiomeDictionary.Type t : types)
+                {
+                    biomesAndTypes.put(t, b);
+                }
+            }
+         EntityRegistry.addSpawn(EntityIceologer.class, ModConfig.iceologerSpawnProbability, ModConfig.iceologerMinSpawnGroup, odConfig.iceologerMaxSpawnGroup, EnumCreatureType.MONSTER, biomesAndTypes.get(BiomeDictionary.Type.SNOWY).toArray(new Biome[biomesAndTypes.get(BiomeDictionary.Type.SNOWY).size()]));
+         EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID,"IceCube"), EntityIceCube.class, "IceCube", id++, Main.instance, 70, 3,true);
 
     }
     @SideOnly(Side.CLIENT)
